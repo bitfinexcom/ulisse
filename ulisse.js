caa65a5532683b109c2e8c47e6e860150e26f029
prdn <paolo.ardoino@gmail.com>
2020-04-12 07:51:06 +0000

    mc

--- ulisse.js
+++ ulisse.js
@@ -51,7 +51,7 @@ function flush () {
       return
     }
 
-    const data = evts.splice(0, 50)
+    const data = evts.splice(0, 100)
 
     for (let i = 0; i < data.length; i++) {
       rpl.push(['publish', conf.dest, JSON.stringify(data[i])])
@@ -65,7 +65,7 @@ function flush () {
 
   pubRc.pipeline(rpl).exec(() => {
     const relaxed = (Date.now() - ulisse._snapTs) < (conf.snapRelaxedFlushTimeout || 30000)
-    setTimeout(flush, relaxed ? 50 : 15)
+    setTimeout(flush, relaxed ? 50 : 10)
   })
 }
 
